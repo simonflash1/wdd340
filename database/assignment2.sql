dddbab15553f1358f88cf596802c91880462508e
simonflash1 <nestor.emmanuel.lizarraga@gmail.com>
2025-01-31 16:17:48 -0300

    database

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -230,3 +230,43 @@ VALUES (
         5
     );
 
+
+
+
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+DELETE FROM account
+WHERE account_id = 1;
+
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_id = 10;
+
+SELECT inv_make,
+    inv_model,
+    classification_name
+FROM classification
+    INNER JOIN inventory ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+
+UPDATE inventory 
+SET inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
